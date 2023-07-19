5873913d5ca03999b839555aca04c2dba43b121c
Nana-Boaheng1 <salimbineddy16@gmail.com>
2023-07-19 03:33:10 +0000

    First Commit

---
+++ src/smartnotepad/JavaApplication14.java
@@ -0,0 +1,171 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication14;
+/**
+ *
+ * @author Midnyt
+ */
+public class JavaApplication14 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         
+//        System.out.println("Enter your age : ");
+//        Scanner sc = new Scanner(System.in);
+//        int score=sc.nextInt();
+//        
+//      
+//     
+//        if (score>=80 && score<=100 ){
+//        System.out.println("Your grade is A . Excellent");
+//        
+//        }
+//        else if (score>=70 && score<=79 ){
+//        System.out.println("Your grade is B . Very Good");
+//        
+//        }
+//        else if (score>=60 && score<=69 ){
+//        System.out.println("Your grade is C . Good");
+//        
+//        }
+//        if (score>=50 && score<=59 ){
+//        System.out.println("Your grade is D . Pass");
+//        
+//        }
+//        else {
+//          System.out.println("Your grade is E . Fail");  
+//        }
+    
+
+//    while(true){
+//         System.out.println("Do you want to loop :");
+//         Scanner sc = new Scanner(System.in);
+//         int loop=sc.nextInt();
+//         if (loop==1){
+//             int number=1;
+//         System.out.println(++number);
+//         }
+//         else{
+//             break;
+//         }
+//    
+//    
+//    }
+
+    
+//    Scanner sc = new Scanner(System.in);
+//    double height=sc.nextDouble();
+//    double weight=sc.nextDouble();
+//    
+//    double bmi=weight/(height*height);
+//    System.out.println("Enter your height  : "+bmi);
+//    if (bmi>=30){
+//        System.out.println("Your bmi is "+bmi+" you are obese");
+//    }
+//    else if (bmi>=25.0 && bmi<=29.9){
+//        System.out.println("Your bmi is "+bmi+" you are Overheight");
+//    }
+//    if (bmi>=18.5 && bmi<=24.9){
+//        System.out.println("Your bmi is "+bmi+" you are Normal");
+//    }
+//    else{
+//        System.out.println("Your bmi is "+bmi+" you are Underweight");
+//    }
+//    
+//System.out.println("************************************");
+//
+//System.out.println("1.  Send Money");
+//System.out.println("2.  Withdraw Cash");
+//System.out.println("3.  Buy Airtime or data");
+//System.out.println("4.  Exist");
+//     
+//System.out.println("************************************");        
+//
+//System.out.println("Please select menu : ");
+//Scanner sc = new Scanner(System.in);
+//int score=sc.nextInt();
+//
+//switch( score){
+//
+//    case 1:System.out.println(" Sent a Money");
+//    case 2:System.out.println(" withdraw a Money");
+//    case 3:System.out.println(" bought airtime");
+//    case 4:
+//        System.out.println(" Thank for using Mtn");
+//        System.exit(0);
+//        
+//    
+//}       
+//                
+//   
+//boolean done=false;
+//int n=5;
+//while(n>0 && !done){
+//    --n;
+//     
+//    
+// 
+//    System.out.println(n); 
+//     done=(n*n==1||n<3);   
+//        
+//        
+//    }
+//Scanner sc=new Scanner(System.in);
+//
+//int number;
+//int cn;
+//boolean i=true;
+//while(i)   {
+//System.out.println("************************");
+//System.out.println("Enter a number to square :");
+//number=sc.nextInt();
+//System.out.println("press 1 to quit");
+//System.out.println("************************");
+//
+//System.out.println("************************");
+//System.out.println("The square of the number is : "+Math.pow(number,2));
+//System.out.println("press 1 to quit");
+//System.out.println("press 2 to continue");
+//cn=sc.nextInt();
+//System.out.println("************************");
+//if (cn==2){
+//    i=true;
+//}
+//else{
+//    i=false;
+//}
+//
+//}
+//
+// for (int i =1;i<=5;i++){
+// System.out.println(i);
+// }
+//int age[]= new int[5];
+//age[0]=1;
+//age[1]=2;
+//age[2]=3;
+//age[3]=4;
+//
+//System.out.print(age[4]);
+
+
+int [] age =new int[5];
+age [0] = 1;
+age [2] = 3;
+age [4] = 5;
+
+for(int i = 0;i<=5; i++) {
+   
+    System.out.print(i);
+    
+}
+
+
+    }  
+}
+
